b2f0901b15c2e922be6c76dc1e8a557651e60bce
fpcrtrlbot <ferdiocal@hotmail.com>
2026-06-27 01:03:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 import requests
 import os
+from datetime import datetime
+
+URL = "https://appointment.as-visa.com/tr/istanbul-bireysel-basvuru"
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-print("bot çalıştı")
 
-requests.post(
-    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-    data={"chat_id": CHAT_ID, "text": "🧪 TEST MESAJI GELDİ"}
-)
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
+
+
+def check_site():
+    try:
+        r = requests.get(URL, timeout=20)
+        html = r.text.lower()
+
+        if "uygun randevu kotası bulunmamaktadır" in html:
+            return False
+
+        return True
+
+    except:
+        return None
+
+
+def run():
+    status = check_site()
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    if status is True:
+        msg = f"🟢 Kontrol: {now}\n📍 Portekiz randevu AÇIK olabilir!"
+    elif status is False:
+        msg = f"🔴 Kontrol: {now}\n📍 Randevu kapalı"
+    else:
+        msg = f"⚠️ Kontrol: {now}\n📍 Hata oluştu"
+
+    send_message(msg)
+
+
+if __name__ == "__main__":
+    run()
